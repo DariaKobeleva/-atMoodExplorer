569bbf59292a530f58c216fd015a71a05c5cfd2e
KDaria <dkdva@mail.ru>
2024-03-20 20:43:06 +0100

    Added image

--- СatMoodExplorer/ViewControllers/MainViewController.swift
+++ СatMoodExplorer/ViewControllers/MainViewController.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
     @IBOutlet var catImage: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    // MARK: - Private Properties
+    private let networkManager = NetworkManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
     }
-
-
+    //MARK: - Private Methods
+    private func fetchImage() {
+        networkManager.fetchImage(from: Link.image.url) { [unowned self] result in
+            switch result {
+            case .success(let imageData):
+                catImage.image = UIImage(data: imageData)
+                activityIndicator.stopAnimating()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
